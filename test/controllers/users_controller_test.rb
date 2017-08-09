77c0e5ed190fb029a8733ef8a8a6659754d0d95d
tfb34 <britothalia212@gmail.com>
2017-08-08 20:08:21 -0400

    Test User controller

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,9 +1,31 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
+  def setup
+  	@user = @user = User.new(name: "Teresa", email: "sushi@gmail.com", birthdate: Date.today)
+  	@user.save
+  end
+
   test "should get new" do
-    get users_new_url
+    get new_user_path
     assert_response :success
   end
+
+  test "should get index" do 
+  	get users_path
+  	assert_response :success
+  end
+
+  test "should get show" do 
+
+  	get user_path(@user.id)
+  	assert_response :success
+  end
+
+  test "should get edit" do 
+  	get edit_user_path(@user.id)
+  	assert_response :success
+  end
+
 
 end
