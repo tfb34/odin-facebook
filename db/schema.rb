b110b9de7cefe8fb95db9b6a88ce50064414ec81
tfb34 <britothalia212@gmail.com>
2017-09-14 18:35:07 -0400

    Add basic comment model

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170911041450) do
+ActiveRecord::Schema.define(version: 20170914215039) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comment_relationships", force: :cascade do |t|
+    t.text     "content"
+    t.integer  "author_id"
+    t.integer  "post_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["author_id"], name: "index_comment_relationships_on_author_id", using: :btree
+    t.index ["post_id"], name: "index_comment_relationships_on_post_id", using: :btree
+  end
 
   create_table "friend_requests", force: :cascade do |t|
     t.integer  "sender_id"
---
+++ test/models/comment_relationship_test.rb
@@ -0,0 +1,40 @@
+
+
+require 'test_helper'
+
+class CommentRelationshipTest < ActiveSupport::TestCase
+  
+  def setup 
+  	@comment = Comment.new(author_id: users(:Buffy).id, 
+  						   post_id: posts(:sushi).id, 
+  						   content: "so delicious!")
+  end
+
+  test "should be valid" do
+  	assert @comment.valid?
+  end
+
+  test "author should be present" do 
+  	@comment.author_id = nil
+  	assert_not @comment.valid?
+  end
+
+  test "post should be present" do 
+  	@comment.post_id = nil
+  	assert_not @comment.valid?
+  end
+
+  test "content should be present" do 
+  	@comment.content = ""
+  	assert_not @comment.valid?
+  end
+
+  #test "destroyed when author is terminated" do 
+
+  #end
+
+  #test "destroyed when post is destroyed" do 
+
+  #end
+
+end
