c92d4a171a29f96d3f89c2af2775c8099fe16124
tfb34 <britothalia212@gmail.com>
2017-09-19 23:45:49 -0400

    Add user profile pic using gravatar

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -4,4 +4,12 @@ module UsersHelper
 		age-=1 if Date.today < (birthday + age.years)
 		return age
 	end
+
+	#returns an image tag for Gravatar with a gravatar CSS class
+	def gravatar_for(user, options={size: 80})
+		gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+		size = options[:size]
+		gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
+		image_tag(gravatar_url, alt: user.name, class: "gravatar")
+	end
 end
