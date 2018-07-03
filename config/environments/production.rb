40e85ec50c586c54bbf6d27103f8535cf580114e
Thalia <britothalia212@gmail.com>
2018-07-03 17:44:23 -0400

    setup S3 credentials

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,11 +3,11 @@ Rails.application.configure do
   config.paperclip_defaults = {
     storage: :s3,
     s3_credentials: {
-    bucket: ENV['BUCKET_NAME'],
-    access_key_id: ENV['ACCESS_KEY_ID'],
-    secret_access_key: ENV['SECRET_ACCESS_KEY'],
-    s3_region: ENV['REGION'],
-    s3_host_name: ENV['HOST_NAME']
+    bucket: ENV['S3_BUCKET_NAME'],
+    access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+    secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
+    s3_region: ENV['S3_REGION'],
+    s3_host_name: ENV['S3_HOST_NAME']
     }
   }
   # Settings specified here will take precedence over those in config/application.rb.
